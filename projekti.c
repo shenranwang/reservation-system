74c7b6ba4e8c2af2edd4a7d1dcd2c16ee315c337
Shenran Wang <shenran268@gmail.com>
2020-08-08 13:06:35 +0300

    formatted and scanf to fgets

--- projekti.c
+++ projekti.c
@@ -6,39 +6,46 @@
 
 #define MAX_LENGTH 80
 
-void perform_command(char *command) {
-    switch(command[0]) {
-        case 'A':
-            printf("Add reservation!\n");
-            break;
-        case 'D':
-            printf("Delete reservation!\n");
-            break;
-        case 'L':
-            printf("Print calendar!\n");
-            break;
-        case 'W':
-            printf("Save calendar!\n");
-            break;
-        case 'O':
-            printf("Open calendar!\n");
-            break;
-        case 'Q':
-            printf("Quit program!\n");
-            break;
-        default:
-            printf("Invalid command!\n");
-            break;
+int check_n_args(int n_args)
+{
+
+    return 1;
+}
+
+void perform_command(char *command)
+{
+    switch (command[0])
+    {
+    case 'A':
+        printf("Add reservation!\n");
+        break;
+    case 'D':
+        printf("Delete reservation!\n");
+        break;
+    case 'L':
+        printf("Print calendar!\n");
+        break;
+    case 'W':
+        printf("Save calendar!\n");
+        break;
+    case 'O':
+        printf("Open calendar!\n");
+        break;
+    case 'Q':
+        printf("Quit program!\n");
+        break;
+    default:
+        printf("Invalid command!\n");
+        break;
     }
 }
 
-void parse_command() {
+void parse_command()
+{
     char command[MAX_LENGTH];
-
-    do {
-        scanf("%s", command);
+    do
+    {
+        fgets(command, 80, stdin);
         perform_command(command);
     } while (command[0] != 'Q');
-
 }
-
